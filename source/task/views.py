be9bc3dfb9e342660e6169bba96a4233e0ac8c05
b3kb91 <19kani92@gmail.com>
2025-01-17 20:51:51 +0600

    delete full project, had problems with naming
    Signed-off-by: b3kb91 <19kani92@gmail.com>

--- source/task/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from test_work.task.models import Task
-from test_work.task.serializers import TaskSerializer
-
-
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
